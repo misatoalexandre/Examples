e9e79d26b1e227e14170659628dba51656ad0f2f
Misato Alexandre <misatotina.alexandre@gmail.com>
2013-07-04 16:10:23 -0400

    Initial Commit

---
+++ Examples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Examples
+//
+//  Created by Misato Tina Alexandre on 7/4/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Examples/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Examples
+//
+//  Created by Misato Tina Alexandre on 7/4/13.
+//  Copyright (c) 2013 Misato Tina Alexandre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
